6a311048d1bdd4bae54286f46bdaacd1033d3a63
martinguido <guidomartin7@gmail.com>
2022-03-30 15:29:23 -0300

    delete classes for component

--- client/src/classes.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
- export class Start extends React.Component {
-    render(){
-        return <h2>Hi, I am a Carlos!</h2>;
-    }
-}
---
+++ client/src/components.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+ export class Start extends React.Component {
+    render(){
+        return <h2>Hi, I am a Carlos!</h2>;
+    }
+}
+export class Header extends React.Component {
+    render(){
+        return <h2>Hi, I am a Carlos!</h2>;
+    }
+}
+
+export class IndexBody extends React.Component {
+    render(){
+        return <h2>Hi, I am a Carlos!</h2>;
+    }
+}
