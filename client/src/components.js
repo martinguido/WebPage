a42b2aa5f50f95b980777723dcfb23cf13dc9f64
martinguido <guidomartin7@gmail.com>
2022-03-31 13:57:57 -0300

    added header to components

--- client/src/components.js
+++ client/src/components.js
@@ -2,17 +2,55 @@ import React from 'react';
 
  export class Start extends React.Component {
     render(){
-        return <h2>Hi, I am a Carlos!</h2>;
+        return <>
+        <Header />
+        <Menu />
+        </>;
     }
 }
 export class Header extends React.Component {
     render(){
-        return <h2>Hi, I am a Carlos!</h2>;
+        return <><div>
+                <h1>Fundación Arbolar</h1>
+            </div>
+            <div>
+                <img src='logo.png'></img>
+            
+            </div>
+        </>;
     }
 }
 
-export class IndexBody extends React.Component {
+export class Menu extends React.Component {
     render(){
-        return <h2>Hi, I am a Carlos!</h2>;
+        return <>
+        <div> 
+            <ul>
+                <li>Inicio</li>
+                <li>Conocenos</li>
+                <ul>
+                    <li>Mision, Vision y Objetivo</li>
+                    <li>Notas</li>
+
+                </ul>
+                <li>Campañas</li>
+                <ul>
+                    <li>Consejos Verdes</li>
+                    <li>Nuestros Logros</li>
+                </ul>
+                <li>Actividades</li>
+                <li>Noticias</li>
+                <li>Voluntariado</li>
+                <li>Testimonios</li>
+                <li>Donaciones</li>
+                <li>Contactanos</li>
+                <ul>
+                    <li>Preguntas Frecuentes</li>
+                    <li>Conocenos en otras redes</li>
+                    <li>Dejanos tu Consulta</li>
+                </ul>
+            </ul>
+        </div>
+    </>;
     }
 }
