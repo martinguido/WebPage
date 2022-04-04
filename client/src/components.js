e3beb9cae73e4ac5ae836c2263085050ad26af06
martinguido <guidomartin7@gmail.com>
2022-04-04 20:31:39 -0300

    finalizing css for newsletter with flex method

--- client/src/components.js
+++ client/src/components.js
@@ -80,13 +80,13 @@ export function SubscribeNewsletter(){
 		    <input type="name" class="form__name" placeholder="Ingresa tu nombre" />
 	    </div>
         <div class="formSecondRow">
-            <div class="form">
+            <div class="form2">
 	            <input type="email" class="form__email" placeholder="Ingresa tu direccion de email" />
+                <div class="notice">
+	                <input type="checkbox"/>
+		            <span class="notice__copy">Estoy de acuerdo con subscribirme al newsletter mensual.</span>
+	            </div>
 	            <button class="form__button">Subscribite</button>
-	        </div>
-	        <div class="notice">
-	            <input type="checkbox"/>
-		        <span class="notice__copy">Estoy de acuerdo con subscribirme al newsletter mensual.</span>
 	        </div>
         </div>
     </div>  
