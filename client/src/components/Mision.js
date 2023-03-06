b9ec84f8d98b5026ab90fc6882ba7e8c18c3b751
Martin Guido <guidomartin7@gmail.com>
2023-03-05 21:50:30 -0300

    code cleaning

--- client/src/components/Mision.js
+++ client/src/components/Mision.js
@@ -7,52 +7,6 @@ export default function Mision() {
         Nuestras acciones están enmarcadas en los Objetivos de Desarrollo
         Sostenible de las Naciones Unidas.
       </h1>
-      {/*<div className="start">
-        <h1>Mision</h1>
-        <p>
-          Nuestra organización tiene como objetivo principal la reforestación y
-          restauración de áreas degradadas y deforestadas en todo el mundo,
-          promoviendo la biodiversidad y el equilibrio ecológico. Trabajamos con
-          comunidades locales, gobiernos y otros actores relevantes para
-          implementar proyectos de reforestación sostenibles y de largo plazo,
-          que incluyen la capacitación y el empoderamiento de las comunidades
-          locales en la gestión y cuidado de los bosques. Además, trabajamos en
-          la promoción de políticas públicas y la conciencia ambiental para
-          reducir la deforestación y promover la restauración ecológica. Nuestro
-          objetivo final es contribuir a la mitigación del cambio climático y la
-          conservación de la naturaleza para las generaciones presentes y
-          futuras.
-        </p>
-      </div>
-      <div className="start">
-        <h1>Vision</h1>
-        <p>
-          Nuestra organización aspira a un mundo en el que los bosques y la
-          biodiversidad sean protegidos y restaurados, y donde las comunidades
-          locales estén empoderadas para la gestión sostenible de los recursos
-          naturales. Queremos ser líderes en la reforestación y restauración
-          ecológica, trabajando en todo el mundo para impulsar la transformación
-          hacia una economía verde y sostenible. Nuestro trabajo es fundamental
-          para reducir la huella de carbono de la humanidad y mitigar los
-          efectos del cambio climático. Además, queremos ser un ejemplo de una
-          organización que integra el enfoque de género y la inclusión social en
-          todas nuestras acciones y proyectos, promoviendo la igualdad de
-          oportunidades y el respeto por los derechos humanos.
-        </p>
-      </div>
-      <div className="start">
-        <h1>Objetivos</h1>
-        <p>
-          Contribuir con organizaciones y empresas con el objetivo de promover
-          el desarrollo sostenible. Generar actividades solidarias para el
-          desarrollo de un mundo mejor en pos de las generaciones actuales y
-          futuras. Mejorar la calidad del hábitat de las instituciones y los
-          espacios verdes circundantes mediante la valorización y la plantación
-          de distintas especies forestales y plantas nativas. Promover la
-          difusión y comunicación de las experiencias a nivel institucional y en
-          medios de comunicación.
-        </p>
-      </div> */}
       <section className="secMision">
         <div className="mision1">
           <h2 className="misionSubtitle">Mision</h2>
