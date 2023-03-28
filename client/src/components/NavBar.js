d0b39a73bc2b03b8fb0789be81ecbe46f5432f4e
Martin Guido <guidomartin7@gmail.com>
2023-03-28 10:37:50 -0300

    added navbar css

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -7,59 +7,67 @@ const NavBar = () => {
       <nav className="dropDownMenu">
         <ul>
           <li>
-            <h1>
-              <Link to="/" className="links">Home</Link>
-            </h1>
+            <Link to="/" className="links">
+              Home
+            </Link>
           </li>
           <li>
-            <h1>Conocenos</h1>
+            <h1 className="mainH1">Conocenos</h1>
             <ul id="subMenu">
               <li>
-                <h1>
-                  <Link to="/Mision" className="links">Mision</Link>
-                </h1>
+                <Link to="/Mision" className="links">
+                  Mision
+                </Link>
               </li>
               <li>
-                <h1>
-                  <Link to="/Testimonios" className="links">Testimonios</Link>
-                </h1>
+                <Link to="/Testimonios" className="links">
+                  Testimonios
+                </Link>
               </li>
               <li>
-                <h1>
-                  <Link to="/Aliados" className="links">Aliados</Link>
-                </h1>
+                <Link to="/Aliados" className="links">
+                  Aliados
+                </Link>
               </li>
             </ul>
           </li>
           <li>
-            <h1>Actividades</h1>
+            <h1 className="mainH1">Actividades</h1>
             <ul id="subMenu">
               <li>
-                <h1>
-                  <Link to="/Voluntarios" className="links">Voluntarios</Link>
-                </h1>
+                <Link to="/Voluntarios" className="links">
+                  Voluntarios
+                </Link>
               </li>
               <li>
-                <h1>
-                  <Link to="/ConsejosVerdes" className="links">Consejos Verdes</Link>
-                </h1>
+                <Link to="/ConsejosVerdes" className="links">
+                  Consejos Verdes
+                </Link>
               </li>
             </ul>
           </li>
           <li>
-            <h1><Link to="/Donaciones" className="links">Donaciones</Link></h1>
+            <Link to="/Donaciones" className="links">
+              Donaciones
+            </Link>
           </li>
           <li>
-            <h1>Contactanos</h1>
+            <h1 className="mainH1">Contactanos</h1>
             <ul id="subMenu">
               <li>
-                <h1><Link to="/FAQ" className="links">FAQ</Link></h1>
+                <Link to="/FAQ" className="links">
+                  FAQ
+                </Link>
               </li>
               <li>
-                <h1><Link to="/Redes" className="links">Redes</Link></h1>
+                <Link to="/Redes" className="links">
+                  Redes
+                </Link>
               </li>
               <li>
-                <h1><Link to="/Contactanos" className="links">Contactanos</Link></h1>
+                <Link to="/Contactanos" className="links">
+                  Contactanos
+                </Link>
               </li>
             </ul>
           </li>
