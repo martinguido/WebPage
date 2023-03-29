882217550d3df427d2a50275cd2cc0dfbc53ab31
Martin Guido <guidomartin7@gmail.com>
2023-03-29 16:42:24 -0300

    correction

--- WebApp/src/main/java/com/arbolar/WebApp/Controllers/SubscriptorsController.java
+++ WebApp/src/main/java/com/arbolar/WebApp/Controllers/SubscriptorsController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
