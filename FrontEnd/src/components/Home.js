b553667cc01d757a233db1c4c06ff34241e26c13
Martin Guido <guidomartin7@gmail.com>
2023-03-30 11:13:50 -0300

    landing home page finished with css

--- FrontEnd/src/components/Home.js
+++ FrontEnd/src/components/Home.js
@@ -3,8 +3,23 @@ import React from "react";
 const Home = () => {
   return (
     <div className="start">
-      <h1>Home</h1>
-    </div>
+      <div className="bgHome__1">
+        <div className="glass">
+          <h1 className="homeTitle">Medioambiente</h1>
+        </div>
+      </div>
+      <div className="glass"></div>
+      <div className="bgHome__2">
+        <div className="glass">
+          <h1 className="homeTitle">Reforestacion</h1>
+        </div>
+      </div>
+      <div className="bgHome__3">
+        <div className="glass">
+          <h1 className="homeTitle">Sustentabilidad</h1>
+        </div>
+      </div>
+    </div >
   );
 };
 
