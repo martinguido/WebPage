c1e7322a9b8dc41f8af82800381c50c6a362801f
Martin Guido <guidomartin7@gmail.com>
2023-04-27 10:47:22 -0300

    modify basic structure of admin frontend

--- AdminFrontEnd/src/components/App.js
+++ AdminFrontEnd/src/components/App.js
@@ -1,42 +1,22 @@
 import React from "react";
-
-import Home from "./Home.js";
 import Header from "./Header.js";
-import NavBar from "./NavBar.js";
-import Mision from "./Mision.js";
-import Testimonios from "./Testimonios.js";
-import Aliados from "./Aliados.js";
-import Voluntarios from "./Voluntarios.js";
-import ConsejosVerdes from "./ConsejosVerdes";
-import Donaciones from "./Donaciones.js";
-import FAQ from "./FAQ.js";
-import Redes from "./Redes.js";
-import Contactanos from "./Contactanos.js";
-import { Route, Routes } from "react-router-dom";
+import { DataGrid } from '@mui/x-data-grid';
 
 const App = () => {
   return (
     <div>
       <header className="topHeader">
         <Header />
-        <NavBar />
       </header>
       <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Mision" element={<Mision />} />
-          <Route path="/Testimonios" element={<Testimonios />} />
-          <Route path="/Aliados" element={<Aliados />} />
-          <Route path="/Voluntarios" element={<Voluntarios />} />
-          <Route path="/ConsejosVerdes" element={<ConsejosVerdes />} />
-          <Route path="/Donaciones" element={<Donaciones />} />
-          <Route path="/FAQ" element={<FAQ />} />
-          <Route path="/Contactanos" element={<Contactanos />} />
-        </Routes>
+        <div>
+          <DataGrid></DataGrid>
+        </div>
+        <div >
+          <DataGrid></DataGrid>
+        </div>
+
       </div>
-      <footer>
-        <Redes />
-      </footer>
     </div>
   );
 };
--- AdminFrontEnd/src/index.js
+++ AdminFrontEnd/src/index.js
@@ -4,14 +4,11 @@ import App from "./components/App.js";
 import "./css/Components.css";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
 
