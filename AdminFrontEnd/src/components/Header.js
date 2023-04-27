5a41f1f3146ea30d18efbc78bc638acf496af4d4
Martin Guido <guidomartin7@gmail.com>
2023-04-27 10:39:10 -0300

    add admin frontend basic app

---
+++ AdminFrontEnd/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import logo from "../img/logo.png";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <h1>Fundación Arbolar</h1>
+      </div>
+      <div className="logoHeader">
+        <img src={logo} alt="Logo Fundacion Arbolar" className="blurred" />
+        <img src={logo} alt="Logo Fundacion Arbolar" className="logoA" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ AdminFrontEnd/src/components/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="start">
+      <div className="bgHome__1">
+        <div className="glass">
+          <h1 className="homeTitle">Medioambiente</h1>
+        </div>
+      </div>
+      <div className="glass"></div>
+      <div className="bgHome__2">
+        <div className="glass">
+          <h1 className="homeTitle">Reforestacion</h1>
+        </div>
+      </div>
+      <div className="bgHome__3">
+        <div className="glass">
+          <h1 className="homeTitle">Sustentabilidad</h1>
+        </div>
+      </div>
+    </div >
+  );
+};
+
+export default Home;
