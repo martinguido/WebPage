1ebbadc296569c6b56f5070aa5e9b64acdc34cd6
Martin Guido <guidomartin7@gmail.com>
2023-12-31 15:12:20 -0300

    exclude daos from testing

--- FrontEnd/src/components/app/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test('App contains a div with className "appHeader"', () => {
-    render(<App />);
-    const appDiv = screen.getByTestId("appHeader");
-    expect(appDiv).toBeInTheDocument();
-});
