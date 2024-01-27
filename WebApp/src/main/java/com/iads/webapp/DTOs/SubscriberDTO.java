a7fb38e2d52813f77b4645912dc9dffa8c1fe5f2
Martin Guido <guidomartin7@gmail.com>
2024-01-27 15:01:10 -0300

    first commit

--- WebApp/src/main/java/com/iads/webapp/DTOs/SubscriberDTO.java
+++ WebApp/src/main/java/com/iads/webapp/DTOs/SubscriberDTO.java
@@ -2,8 +2,6 @@ package com.iads.webapp.DTOs;
 
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
 public class SubscriberDTO {
 
--- WebApp/src/main/java/com/iads/webapp/Services/SubscriberService.java
+++ WebApp/src/main/java/com/iads/webapp/Services/SubscriberService.java
@@ -5,9 +5,6 @@ import com.iads.webapp.DTOs.SubscriberDTO;
 import com.iads.webapp.Repositories.SubscriberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
 
@@ -16,13 +13,15 @@ import java.util.List;
 public class SubscriberService {
     private final SubscriberRepository subscriberRepository;
 
-    public List<Subscriber> getAllSubscribers() {return (List<Subscriber>) subscriberRepository.findAll();
+    public List<Subscriber> getAllSubscribers() {
+        return (List<Subscriber>) subscriberRepository.findAll();
     }
 
     public void createSubscriber(SubscriberDTO subscriberDTO) {
         Subscriber aNewSubscriber = new Subscriber(subscriberDTO.getName(), subscriberDTO.getMail(), new Date());
         subscriberRepository.save(aNewSubscriber);
     }
+
     public void saveAll(List<Subscriber> someSubscribers) {
         subscriberRepository.saveAll(someSubscribers);
     }
